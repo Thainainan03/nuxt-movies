5ca894629650be0b39c3da436136c97844d7a509
Thanainan <toypoyomn03@outlook.com>
2024-03-08 09:39:16 +0700

    refactor

--- pages/test-image/index.vue
+++ pages/test-image/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <img src="/public/mirai.gif" alt="test image">
-    <img src="/public/31dec.gif" alt="test image">
-    <img src="/public/reward-1.jpeg" alt="test image">
-    <img src="/public/er.jpg" alt="test image">
+  <div class="flex">
+    <img src="/public/mirai.gif" class="w-1/5" alt="test image">
+    <img src="/public/31dec.gif" class="w-1/5" alt="test image">
+    <img src="/public/reward-1.jpeg" class="w-1/5" alt="test image">
+    <img src="/public/er.jpg" class="w-1/5" alt="test image">
+    <img src="/public/social-card.png" class="w-1/5" alt="test image">
   </div>
 </template>
