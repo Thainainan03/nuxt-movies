0f31d1501fa8515ae3e64a60076e8e507d042c69
Thanainan <toypoyomn03@outlook.com>
2024-03-08 09:32:10 +0700

    test image load

---
+++ pages/test-image/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <img src="/public/mirai.gif" alt="test image">
+    <img src="/public/31dec.gif" alt="test image">
+    <img src="/public/reward-1.jpeg" alt="test image">
+    <img src="/public/er.jpg" alt="test image">
+  </div>
+</template>
